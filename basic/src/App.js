62ea469fa62b50948af8cc7acc28a53d7efe937a
userzenox <piyush.132kp@gmail.com>
2024-06-22 21:11:52 +0530

    day-22

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,7 +1,23 @@
-
 function App() {
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+  let char = Math.floor(Math.random() * str.length )
+  console.log(char);
+
+
+  let length=8
+  let pass = ""
+  for (let i = 1; i <=length; i++) {
+    let char = Math.floor(Math.random() * str.length + 1 )
+       pass += str.charAt(char)
+   }
+   console.log(pass);
+  
   return (
+    <>
     <h1> React study</h1>
+   
+    </>
   );
 }
 
