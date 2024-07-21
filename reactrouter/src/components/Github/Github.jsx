c875297a67adc4f92969a4a49f9bf8e1b25b0ab1
userzenox <piyush.132kp@gmail.com>
2024-07-21 23:27:53 +0530

    7th commit

--- reactrouter/src/components/Github/Github.jsx
+++ reactrouter/src/components/Github/Github.jsx
@@ -1,15 +1,17 @@
 import React, {useState , useEffect} from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-      fetch('https://api.github.com/users/userzenox')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setData(data)
-      })
-    }, [])
+
+  const data = useLoaderData()
+//     const [data, setData] = useState([])
+//     useEffect(() => {
+//       fetch('https://api.github.com/users/userzenox')
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+//         setData(data)
+//       })
+//     }, [])
     
   return (
     <div className='text-center m-4 bg-gray-500 text-3xl p-4'>Github followers: {data.followers}
@@ -19,3 +21,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+   const response = await fetch('https://api.github.com/users/userzenox')
+   return response.json()
+}
